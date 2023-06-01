5c36d6f94bc46182bac8031b9670b85b62dd2fb9
Erik Russell <erikrussells@gmail.com>
2023-06-01 12:26:06 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Kaltrunner-band
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+  return (
+    <>
+      <h1>Kaltrunner</h1>
+    </>
+  )
+}
+
+export default App
