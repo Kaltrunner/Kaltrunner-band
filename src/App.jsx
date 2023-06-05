a9af1229af847180097236338a6b5f784b073243
Erik Russell <erikrussells@gmail.com>
2023-06-05 16:45:06 -0400

    added cursor and deployed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,49 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import AnimatedCursor from "react-animated-cursor";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Body from "./assets/components/body/Body";
 
 function App() {
-
   return (
     <>
-      <h1>Kaltrunner</h1>
+      <AnimatedCursor
+        id="mouse"
+        innerSize={110}
+        outerSize={0}
+        color="255, 255, 255"
+        outerAlpha={0.1}
+        innerScale={0.5}
+        outerScale={0}
+        hasBlendMode={true}
+        trailingSpeed={15}
+        innerStyle={{
+          mixBlendMode: "difference",
+        }}
+        outerStyle={{
+          mixBlendMode: "difference",
+        }}
+        clickables={[
+          "a",
+          'input[type="text"]',
+          'input[type="email"]',
+          'input[type="number"]',
+          'input[type="submit"]',
+          'input[type="image"]',
+          'input[type=""]',
+          "label[for]",
+          "select",
+          "textarea",
+          "button",
+          ".link",
+        ]}
+      />
+
+      <Routes>
+        <Route path="/" element={<Body />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/assets/components/body/Body.jsx
@@ -0,0 +1,11 @@
+import "./body.css";
+
+function Body() {
+    return (
+        <div className="main-body-div" >
+            <h1 className="header-text" >Kaltrunner</h1>
+        </div>
+    )
+}
+
+export default Body;
