6cca9c3fa4f7fd150d14dd6a288221da1a520998
Erik Russell <erikrussells@gmail.com>
2023-06-06 14:22:05 -0400

    added a bg-vid

--- src/assets/components/body/Body.jsx
+++ src/assets/components/body/Body.jsx
@@ -1,11 +1,35 @@
 import "./body.css";
+import React, { useRef, useEffect } from 'react';
 
 function Body() {
-    return (
-        <div className="main-body-div" >
-            <h1 className="header-text" >Kaltrunner</h1>
+
+    const vidRef = useRef(null);
+
+    useEffect(() => {
+        if (vidRef.current) {
+          vidRef.current.playbackRate = 0.1;
+        }
+      }, []);
+
+  return (
+    <>
+      <div className="main-body-div">
+        <div className="overlay" ></div>
+        <video
+         ref={vidRef}
+          id="bg-vid"
+          src="Untitled.mp4"
+          type="video/mp4"
+          autoPlay
+          loop
+          muted
+        />
+        <div className="body-text-div">
+          <h1 className="header-text">Kaltrunner</h1>
         </div>
-    )
+      </div>
+    </>
+  );
 }
 
 export default Body;
