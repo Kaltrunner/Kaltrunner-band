8e2bd46395646f95503bf5b264bfb014247a1e88
Erik Russell <erikrussells@gmail.com>
2023-06-25 12:28:18 -0400

    fixed scroll issue

--- src/assets/components/about/About.jsx
+++ src/assets/components/about/About.jsx
@@ -1,20 +1,26 @@
 import "./about.css";
+import React, { useEffect } from "react";
 
 function About() {
-
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      // console.log(entry)
-      if (entry.isIntersecting) {
-        entry.target.classList.add("show");
-      } else {
-        entry.target.classList.remove("show");
-      }
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+        } else {
+          entry.target.classList.remove("show");
+        }
+      });
     });
-  });
 
-  const hiddenElement = document.querySelectorAll(".hidden");
-  hiddenElement.forEach((element) => observer.observe(element));
+    const hiddenElements = document.querySelectorAll(".hidden");
+    hiddenElements.forEach((element) => observer.observe(element));
+
+    return () => {
+      hiddenElements.forEach((element) => observer.unobserve(element));
+    };
+  }, []);
 
   return (
     <div className="body-about-section">
