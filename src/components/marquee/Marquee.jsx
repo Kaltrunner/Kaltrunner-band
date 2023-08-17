c7d44785d94bd884abbb96d7c9dcf1d098d70afd
Erik Russell <erikrussells@gmail.com>
2023-08-17 15:56:11 -0400

    small style update

--- src/components/marquee/Marquee.jsx
+++ src/components/marquee/Marquee.jsx
@@ -4,16 +4,16 @@ function Marquee() {
   return (
     <div className="marquee">
       <div className="marquee-line">
-        New EP <span className="highlight">"Eye of God"</span> coming soon!
+        New EP <span className="highlight">"Eye of God"</span> coming 10/13/23
       </div>
       <div className="marquee-line">
-        New EP <span className="highlight">"Eye of God"</span> coming soon!
+        New EP <span className="highlight">"Eye of God"</span> coming 10/13/23
       </div>
       <div className="marquee-line">
-        New EP <span className="highlight">"Eye of God"</span> coming soon!
+        New EP <span className="highlight">"Eye of God"</span> coming 10/13/23
       </div>
       <div className="marquee-line">
-        New EP <span className="highlight">"Eye of God"</span> coming soon!
+        New EP <span className="highlight">"Eye of God"</span> coming 10/13/23
       </div>
     </div>
   );
