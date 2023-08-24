b1a452534e85f72d2c2f600d899e84506efc3a92
Erik Russell <erikrussells@gmail.com>
2023-08-24 12:45:29 -0400

    rebuilt the footer to allow better mobile sizing

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,9 +2,86 @@ import "./footer.css";
 
 function Footer() {
   return (
-    <div className="footer-body-div">
-      <div className="line-div"></div>
-      <div className="container text-center">
+    <>
+      <div className="footer-body-div">
+        <div className="footer-info-div">
+          <div className="footer-header">
+            <h6>FOLLOW</h6>
+          </div>
+          <div className="footer-link-div">
+            <a
+              id="footer-link"
+              target="_blank"
+              rel="noreferrer"
+              href="https://open.spotify.com/artist/55PSX5vztaOftFhmVG3Q2D"
+            >
+              SPOTIFY
+            </a>
+          </div>
+          <div className="footer-link-div">
+            <a
+              id="footer-link"
+              target="_blank"
+              rel="noreferrer"
+              href="https://kaltrunner.bandcamp.com/"
+            >
+              BANDCAMP
+            </a>
+          </div>
+          <div className="footer-link-div">
+            {" "}
+            <a
+              id="footer-link"
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.instagram.com/kaltrunner/?hl=en"
+            >
+              INSTAGRAM
+            </a>
+          </div>
+        </div>
+        <div className="footer-info-div">
+          <div className="footer-header">
+            <h6>CONTACT</h6>
+          </div>
+          <div className="footer-link-div">
+            <a
+              id="footer-link"
+              className="nav-link"
+              href="mailto:kaltrunnerband@gmail.com?subject=Hello!"
+            >
+              KALTRUNNERBAND@GMAIL.COM
+            </a>
+          </div>
+        </div>
+        <div className="footer-info-div">
+          <div className="footer-header">
+            <h6>PURCHASE</h6>
+          </div>
+          <div className="footer-link-div" id="last-div">
+            <a
+              id="footer-link"
+              target="_blank"
+              className="nav-link"
+              href="https://www.aurisapothecary.org/shop/kaltrunner-dirteater-ii/"
+            >
+              AURISAPOTHECARY.ORG
+            </a>
+            <div id="copy-text">
+              ©2023 –{" "}
+              <a
+                className="port-link"
+                id="footer-link"
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.erikmrussell.com/"
+              >
+                Erik Russell
+              </a>
+            </div>
+          </div>
+        </div>
+        {/* <div className="container text-center">
         <div className="row">
           <div className="col footer-col-1">FOLLOW</div>
           <div className="col footer-col-1">CONTACT</div>
@@ -81,8 +158,9 @@ function Footer() {
             </a>
           </div>
         </div>
+      </div> */}
       </div>
-    </div>
+    </>
   );
 }
 
