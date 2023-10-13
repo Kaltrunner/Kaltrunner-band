dea8d4ad1e52d69181eef62257d9abe135422083
Erik Russell <erikrussells@gmail.com>
2023-10-13 00:02:35 -0400

    updated marquee

--- src/components/marquee/Marquee.jsx
+++ src/components/marquee/Marquee.jsx
@@ -4,16 +4,16 @@ function Marquee() {
   return (
     <div className="marquee">
       <div className="marquee-line">
-        New EP <span className="highlight">"Eye of God"</span> coming 10/13/23
+        NEW EP <span className="highlight">"Eye of God"</span> OUT NOW!
       </div>
       <div className="marquee-line">
-        New EP <span className="highlight">"Eye of God"</span> coming 10/13/23
+        NEW EP <span className="highlight">"Eye of God"</span> OUT NOW!
       </div>
       <div className="marquee-line">
-        New EP <span className="highlight">"Eye of God"</span> coming 10/13/23
+        NEW EP <span className="highlight">"Eye of God"</span> OUT NOW!
       </div>
       <div className="marquee-line">
-        New EP <span className="highlight">"Eye of God"</span> coming 10/13/23
+        NEW EP <span className="highlight">"Eye of God"</span> OUT NOW!
       </div>
     </div>
   );
