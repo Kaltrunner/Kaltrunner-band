8a0cf386a17b9e80d7dd71260f376bd81da94207
Erik Russell <erikrussells@gmail.com>
2024-04-15 12:36:56 -0400

    trying to get this to work

--- src/components/videos/Videos.jsx
+++ src/components/videos/Videos.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e9bcf18fa26a51233908d537e7319f75d2d80666916b1f1249e6063d3f85b5cd
-size 1215
+oid sha256:82e28557aca3e67ead33652b55c52feaf44a30d4f8dbab8b903289421d35493b
+size 1171
