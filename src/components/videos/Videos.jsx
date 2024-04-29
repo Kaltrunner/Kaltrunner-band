bcdd28f43f62f1fc1b928bdf6e441f31f564ac0b
Erik Russell <erikrussells@gmail.com>
2024-04-29 15:08:31 -0400

    testing a youtube embeded video

--- src/components/videos/Videos.jsx
+++ src/components/videos/Videos.jsx
@@ -9,6 +9,7 @@ function Videos() {
       <div className="videos-content-div">
         <h1 className="videos-header">Audio / Visual<br/> (Coming Soon)</h1>
         <div className="video-div">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/02dTcTkEcfg?si=qd5rewojVee9K8zL" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
           {/* <video className="kaltVideo" controls>
             <source
              
@@ -16,9 +17,9 @@ function Videos() {
             />
             Your browser does not support the video tag.
           </video> */}
-          {/* <p className="video-text">
+          <p className="video-text">
             Audio • "Intruder" taken from the album Dirteater II, originally released
-            july 2021 by: KALTRUNNER <br /> Visual • Filmed & edited february –
+            july 2021 by: KALTRUNNER <br /> Visuals • Filmed & edited february –
             April 2024 by:{" "}
             <a
               className="peter-link-under-line, "
@@ -29,7 +30,7 @@ function Videos() {
             >
               Peter Russell
             </a>
-          </p> */}
+          </p>
         </div>
       </div>
     </div>
