9cd93b6f0d994ed45ed8c35ed9d430ef22087295
Erik Russell <erikrussells@gmail.com>
2024-05-01 16:48:28 -0400

    updated video

--- src/components/videos/Videos.jsx
+++ src/components/videos/Videos.jsx
@@ -9,7 +9,7 @@ function Videos() {
       <div className="videos-content-div">
         <h1 className="videos-header">Audio / Visual</h1>
         <div className="video-div">
-        <iframe src="https://www.youtube.com/embed/02dTcTkEcfg?si=qd5rewojVee9K8zL" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <iframe src="https://www.youtube.com/embed/C6kySCG5sJ8?si=ff_uyYqlBfjfsUNh" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
           {/* <video className="kaltVideo" controls>
             <source
              
